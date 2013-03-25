e75c2a367daccf84074b839ba236f0aa8efb4c77
krasnopolski <krasnopolski@a245ae04-a1c6-4bee-b619-a87606d5c87a>
2013-03-25 03:49:49 +0000

    Initial import.

---
+++ source/trunk/erl.rsrc.pool/src/resource_pool.erl
@@ -0,0 +1,178 @@
+%%
+%% Copyright (C) 2013 by krasnop@bellsouth.net (Alexei Krasnopolski)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License. 
+%%
+
+%% @since 2013-03-21
+%% @copyright 2013 Alexei Krasnopolski
+%% @author Alexei Krasnopolski <krasnop@bellsouth.net> [http://krasnopolski.org/]
+%% @version {@version}
+%% @doc
+%% Facade for resource pool. 
+%%
+
+-module(resource_pool).
+
+%%
+%% Include files
+%%
+-include("settings.hrl").
+
+%% API functions export
+-export([new/3, new/4, borrow/1, return/2, add/1, invalidate/2, get_num_active/1, get_num_idle/1, get_number/1, clear/1, close/1]).
+
+%% @spec new(Pool_name::atom(), Factory_module::atom(), Resource_metadata::term()) -> {ok, Resource} | ignore | {error,Error}
+%% 
+%% @doc Creates and runs new generic server for {@module} with registered name <code>Pool_name</code>. The new resource pool will use
+%% Factory_module as a resource factory and Resource_metadata as a metadata to create a new resource.
+%%
+new(Pool_name, Factory_module, Resource_metadata) ->
+  new(Pool_name, Factory_module, Resource_metadata, []).
+
+%% @spec new(Pool_name::atom(), Factory_module::atom(), Resource_metadata::term(), Options::list({Key, Value})) -> {ok, Resource} | ignore | {error,Error}
+%% 
+%% @doc Creates and runs new generic server for {@module} with registered name <code>Pool_name</code>. The new resource pool will use
+%% Factory_module as a resource factory and Resource_metadata as a metadata to create a new resource.
+%%<p>The available options are:</p>
+%%<dl>
+%%  <dt><code>{max_active, integer()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{max_idle, integer()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{min_idle, integer()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{test_on_borrow, boolean()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{test_on_return, boolean()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{fifo, boolean()}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{when_exhausted_action, (fail | block | grow)}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{max_wait, (integer() | infinity)}</code></dt>
+%%  <dd></dd>
+%%  <dt><code>{max_idle_time, (integer() | infinity)}</code></dt>
+%%  <dd></dd>
+%%</dl>
+new(Pool_name, Factory_module, Resource_metadata, Options) ->
+  FailedOptions = lists:filter(fun({Key, _}) -> not lists:member(Key, 
+    [max_active, max_idle, min_idle, test_on_borrow, test_on_return, fifo, when_exhausted_action, max_wait, max_idle_time]) end, Options),
+  case FailedOptions of
+    [] ->
+      case is_factory(Factory_module) of
+        true ->
+            gen_server:start_link({local, Pool_name}, resource_pool_srv, {Options, Factory_module, Resource_metadata}, [{timeout, ?GEN_SERVER_TIMEOUT}]);
+         {error, _} = Er -> Er
+      end;
+    [{K, _} | T] -> {error, "Wrong options: " ++ lists:concat([K | lists:map(fun({Key, _}) -> ", " ++ atom_to_list(Key) end, T)])}
+  end
+.
+
+%% @spec is_factory(Factory_module) -> boolean()
+%% @doc
+%% @private 
+is_factory(Factory_module) ->
+  Module_info = try Factory_module:module_info(attributes) catch error:undef -> [] end,
+  case Module_info of
+    [] -> {error, factory_not_exist};
+    _ ->
+      case lists:keyfind(behaviour, 1, Module_info) of
+        {behaviour, L} -> 
+          case lists:member(resource_factory, L)  of
+            true -> true; 
+            false -> {error, not_factory}
+          end;
+        false -> {error, not_factory}
+      end
+  end
+.
+
+%% @spec borrow(Pool_name::atom() | pid()) -> Resource | {error, Reason}
+%% 
+%% @doc Borrows resource from pool for client use.
+borrow(Pool_name) ->
+  case gen_server:call(Pool_name, borrow, ?GEN_SERVER_TIMEOUT) of
+    {ok, Resource} -> Resource;
+    {error, pool_exhausted} -> {error, pool_exhausted};
+    {wait, Max_wait} ->
+      Receive =
+      receive
+        {ok, Pid} -> Pid
+      after Max_wait -> {error, pool_timeout}
+      end,
+      gen_server:call(Pool_name, {ack_borrow, Receive}, ?GEN_SERVER_TIMEOUT),
+      %% flush message that probably came to the mailbox after timeout
+      receive
+        {ok, _} -> ok
+      after 0 -> ok
+      end,
+      Receive
+  end
+.
+
+%% @spec return(Pool_name::atom(), Resource::term()) -> ok
+%% 
+%% @doc Returns resource to pool after client does not need it more.
+return(Pool_name, Resource) ->
+  gen_server:cast(Pool_name, {return, Resource, self()})
+.
+
+%% @spec add(Pool_name::atom()) -> ok
+%% 
+%% @doc Adds one more resource to pool (as an idle resource).
+add(Pool_name) ->
+  gen_server:cast(Pool_name, add)
+.
+
+%% @spec invalidate(Pool_name::atom(), Resource::term()) -> ok
+%% 
+%% @doc Invalidates resource - makes it ready to dispose.
+invalidate(Pool_name, Resource) ->
+  gen_server:call(Pool_name, {invalidate, Resource}, ?GEN_SERVER_TIMEOUT)
+.
+
+%% @spec get_num_active(Pool_name::atom()) -> integer()
+%% 
+%% @doc Returns number of active (busy) resource in pool.
+get_num_active(Pool_name) ->
+  gen_server:call(Pool_name, get_num_active, ?GEN_SERVER_TIMEOUT)
+.
+
+%% @spec get_num_idle(Pool_name::atom()) -> integer()
+%% 
+%% @doc Returns number of idle (ready to use) resource in pool.
+get_num_idle(Pool_name) ->
+  gen_server:call(Pool_name, get_num_idle, ?GEN_SERVER_TIMEOUT)
+.
+
+%% @spec get_number(Pool_name::atom()) -> integer()
+%% 
+%% @doc Returns total number of resource in pool.
+get_number(Pool_name) ->
+  gen_server:call(Pool_name, get_number, ?GEN_SERVER_TIMEOUT)
+.
+
+%% @spec clear(Pool_name::atom()) -> ok
+%% 
+%% @doc Disposes all resources from the pool.
+clear(Pool_name) ->
+  gen_server:cast(Pool_name, clear)
+.
+
+%% @spec close(Pool_name::atom()) -> ok
+%% 
+%% @doc Disposes all resources from the pool and close the pool (shut down generic server). 
+close(Pool_name) ->
+  gen_server:cast(Pool_name, close)
+.
