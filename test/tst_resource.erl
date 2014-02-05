e6a0ae632693fec36960063dd56c0504317aeb5d
krasnopolski <krasnopolski@a245ae04-a1c6-4bee-b619-a87606d5c87a>
2014-02-05 22:52:39 +0000

    Everyday routine

--- test/tst_resource.erl
+++ test/tst_resource.erl
@@ -1,5 +1,25 @@
-%% @author alexeikrasnopolski
-%% @doc @todo Add description to tst_resource.
+%%
+%% Copyright (C) 2013-2014 by krasnop@bellsouth.net (Alexei Krasnopolski)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License. 
+%%
+
+%% @hidden
+%% @since 2013-02-20
+%% @copyright 2013-2014 Alexei Krasnopolski
+%% @author Alexei Krasnopolski <krasnop@bellsouth.net> [http://krasnopolski.org/]
+%% @version {@version}
+%% @doc This module is running unit tests for helper_common module.
 
 -module(tst_resource).
 -behaviour(gen_server).
